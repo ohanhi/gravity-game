16ea722f3153396cfcc1a6d4872142d2c70920e0
Ossi Hanhinen <ossi.hanhinen@gmail.com>
2014-09-29 16:40:35 +0300

    Remove unneeded code

--- gravity.js
+++ gravity.js
@@ -1,40 +1,9 @@
 function Gravity() {
 
   var SKETCH_OPTIONS = {
-    centerMass: 100000,
-    partCount: 1000,
-    partMassFactor: 0.2,
-    gravitationalConstant: 1.0
-  };
-
-  var restartWithInputValues = function(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-
-    function floatVal(id) {
-      return parseFloat(document.getElementById(id).value, 10);
-    }
-    SKETCH_OPTIONS.centerMass = floatVal('centerMass');
-    SKETCH_OPTIONS.partCount = parseInt(floatVal('partCount'));
-    SKETCH_OPTIONS.partMassFactor = floatVal('partMassFactor');
-    SKETCH_OPTIONS.gravitationalConstant = floatVal('gravitationalConstant');
-
-    document.getElementById('dialog').className = "closed";
-    restart();
-  };
-
-  document.getElementById('start').onclick = restartWithInputValues;
-  document.getElementById('params').onsubmit = restartWithInputValues;
-
-  document.getElementById('dialogOpen').onclick = function(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-    var dialog = document.getElementById('dialog');
-    if (dialog.className === "open") {
-      dialog.className = "closed";
-    } else {
-      dialog.className = "open";
-    }
+    partCount: 20,
+    partMassFactor: 20,
+    gravitationalConstant: 10.0
   };
 
   function sketchProc(P) {
@@ -117,12 +86,6 @@ function Gravity() {
         P.noStroke();
       }
     };
-    Part.prototype.checkClick = function(x, y) {
-      var dx = this.x - x,
-        dy = this.y - y,
-        result = Math.sqrt(dx*dx + dy*dy) < (this.radius + CLICK_RADIUS);
-      return result;
-    };
     Part.prototype.collide = function(part) {
       // smaller particle always moves, not the larger
       if (part.m > this.m) {
@@ -189,14 +152,6 @@ function Gravity() {
     PartSystem.prototype.add = function(part) {
       this.parts.push(part);
     };
-    PartSystem.prototype.addCentralPart = function() {
-      var part = new Part(
-        CANVAS.width/2, CANVAS.height/2,
-        0, 0,
-        SKETCH_OPTIONS.centerMass
-      );
-      this.parts.push(part);
-    };
     PartSystem.prototype.update = function(draw) {
       var self = this;
 
@@ -242,8 +197,7 @@ function Gravity() {
         P.fill(90);
         P.text("PAUSE", CANVAS.width*0.5, CANVAS.height*0.5);
       } else {
-        P.fill(0,0,0, 40);
-        P.rect(0,0,CANVAS.width,CANVAS.height);
+        P.background(0,0,0, 40);
         this.update(true);
       }
     };
@@ -266,55 +220,11 @@ function Gravity() {
         this.add(part);
       }
     };
-    PartSystem.prototype.createProtoDisk = function(n) {
-      this.addCentralPart();
-
-      var canvasMin = (CANVAS.width < CANVAS.height) ? CANVAS.width : CANVAS.height,
-        gaussianFactor = canvasMin * 0.15;
-        cx = CANVAS.width * 0.5,
-        cy = CANVAS.height * 0.5;
-
-      for (var i = 0; i < n; i++) {
-        var x, y, vx, vy, d, a, v;
-
-        // Get Gaussian random position
-        x = P.randomGaussian() * gaussianFactor;
-        y = P.randomGaussian() * gaussianFactor;
-        d = P.dist(x,y, 0,0);
-
-        // Calculate angle, randomize start velocity along tangent
-        a = P.atan2(y, x) + P.PI,
-        v = Math.random()*8 + 2;
-
-        vx = v * P.sin(a);
-        vy = -1 * v * P.cos(a);
-
-        // Move to canvas centre
-        x += cx;
-        y += cy;
-
-        var part = new Part(
-          x, y,
-          vx, vy,
-          (Math.random()*10 + 3) * SKETCH_OPTIONS.partMassFactor
-        );
-        this.add(part);
-      }
-    };
-    PartSystem.prototype.handleClick = function(x, y) {
-      for (var i = 0; i < this.parts.length; i++) {
-        if (this.parts[i].checkClick(x, y) === true) {
-          ps.setHighlighted(i);
-          return;
-        }
-      }
-      ps.setHighlighted(-1);
     };
     // END DEFINITIONS
 
 
     var ps = new PartSystem();
-    ps.createProtoDisk(SKETCH_OPTIONS.partCount);
 
     P.setup = function() {
       P.size(CANVAS.width, CANVAS.height);
@@ -338,15 +248,12 @@ function Gravity() {
     };
 
     document.onmousedown = function(evt) {
-      ps.handleClick(evt.x, evt.y);
     }
   }
 
   var canvas = document.getElementById("processing-canvas");
   // attaching the sketchProc function to the canvas,
-  // exit immediately to wait for user input
   var processing = new Processing(canvas, sketchProc);
-  processing.exit();
 
   var restart = function() {
     processing.exit();
